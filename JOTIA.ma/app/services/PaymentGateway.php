3679e32cd400120c15b1f7e600d4e318d9eb64c9
Driss_Dell <drisspaca4@gmail.com>
2024-11-21 07:14:31 +0100

    get_next_line

--- JOTIA.ma/app/services/PaymentGateway.php
+++
@@ -1,9 +0,0 @@
-<?php
-class PaymentGateway {
-    public function processPayment($amount, $paymentDetails) {
-        // بوابة دفع مبسطة (مثال - يمكنك التكامل مع PayPal أو Stripe هنا)
-        // لاحظ أن هذا كود مبسط وقد يتطلب دمج API حقيقي مع PayPal أو Stripe
-        return true;  // نجاح الدفع
-    }
-}
-?>

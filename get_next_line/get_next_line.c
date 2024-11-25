ad4da703bb7585b275e5e93f8e8ed432a00ca322
Driss_Dell <drisspaca4@gmail.com>
2024-11-25 11:17:22 +0100

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -15,24 +15,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// char *_fill_line_buffer(int fd, char *left_c, char *buffer)
-// {
 
-// }
-
-char	*fill_stock(char *stock, char *buffer, int fd)
+char	*_fill_stock(char *stock, char *buffer, int fd)
 {
 	char		*tmp;
 	ssize_t		size;
+
 	size = 1;
 	while (size > 0 && !ft_strchr(stock, '\n'))
 	{
 		size = read(fd, buffer, BUFFER_SIZE);
 		if (size < 0)
 		{
-			free(buffer);
 			free(stock);
-			return (NULL); 
+			return (NULL);
 		}
 		buffer[size] = '\0';
 		tmp = stock;
@@ -41,78 +37,61 @@ char	*fill_stock(char *stock, char *buffer, int fd)
 		else
 			stock = ft_strjoin(stock, buffer);
 		free(tmp);
-		if(!stock)
-		{
-			free(buffer);
-			return NULL;
-		}
 	}
 	return stock;
 }
 
-// --------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------
+char *_split_stock(char **stock)
+{
+	char *tmp;
+	char *line;
+
+	if (ft_strchr(*stock, '\n'))
+	{
+		line = ft_substr(*stock, 0, ft_strchr(*stock, '\n') - *stock + 1);
+		tmp = ft_strdup(ft_strchr(*stock, '\n') + 1);
+		free(*stock);
+		*stock = tmp; 
+		if (**stock == '\0')
+		{
+			free(*stock);
+			*stock = NULL;
+		}
+	}
+	else if (*stock != NULL && **stock != '\0')
+	{
+		line = ft_strdup(*stock);
+		free(*stock);
+		*stock = NULL;
+	}
+	else
+		line = NULL;
+
+	return line;
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*stock = NULL;
 	char		*buffer;
 	char		*line;
-	char		*tmp;
-	// ssize_t		size;
 
-	buffer = NULL;
-	line = NULL;
-	tmp = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	stock = fill_stock(stock, buffer, fd);
-	// ---------------------------------------------------
-	// while (size > 0 && !ft_strchr(stock, '\n'))
-	// {
-	// 	size = read(fd, buffer, BUFFER_SIZE);
-	// 	if (size < 0)
-	// 	{
-	// 		free(buffer);
-	// 		return (NULL); 
-	// 	}
-	// 	buffer[size] = '\0';
-	// 	tmp = stock;
-	// 	if (!stock)
-	// 		stock = ft_strdup(buffer);
-	// 	else
-	// 		stock = ft_strjoin(stock, buffer);
-	// 	free(tmp);
-	// }
 
-	// ----------------------------------------------------
-	if (ft_strchr(stock, '\n'))
-	{
-		line = ft_substr(stock, 0, ft_strchr(stock, '\n') - stock + 1);
-		tmp = ft_strdup(ft_strchr(stock, '\n') + 1);
-		free(stock);
-		if (tmp[0] == '\0')
-		{
-			free(tmp);
-			stock = NULL;
-		}
-		else
-			stock = tmp;
-	}
-	else if (stock != NULL && *stock != '\0') 
-	{
-		line = ft_strdup(stock);
-		free(stock);
-		stock = NULL;
-	}
-
-	// -----------------------------------------------------
+	stock = _fill_stock(stock, buffer, fd);
 	free(buffer);
-	return (line);
+	if (!stock)
+		return NULL;
+	line = _split_stock(&stock);
+
+	return line;
 }
 
+// --------------------------------------------------------------------------------
 
 int main()
 {

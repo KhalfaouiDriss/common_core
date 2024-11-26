27236743bc45cd50895b41cffa2f9e448e86d703
Driss Khalfaoui Hassani <dkhalfao@c2r5p12.1337.ma>
2024-11-26 10:21:35 +0100

    get_next_line

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkhalfao <dkhalfao@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 15:09:28 by dkhalfao          #+#    #+#             */
+/*   Updated: 2024/11/20 19:06:45 by dkhalfao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *str);
+
+char	*ft_strchr(const char *str, int c);
+
+char	*ft_strdup(const char *str);
+
+char	*ft_strjoin(char *s1, const char *s2);
+
+char	*ft_substr(const char *str, unsigned int start, size_t n);
+
+#endif
--- get_next_line/get_next_line_fin.c
+++ get_next_line/get_next_line_fin.c
@@ -10,15 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// char *_fill_line_buffer(int fd, char *left_c, char *buffer)
-// {
-
-// }
-
-// char *_set_line(char *line_buffer)
-// {
-
-// }
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <limits.h>

333ded57ce8a45004ab5c630182e2a8f8f5a741b
C47_debian <C47@gmail.com.com>
2025-02-25 12:57:36 +0000

    Common_Core

---
+++ so_long/so_long.h
@@ -0,0 +1,17 @@
+#ifndef SO_LONG
+# define SO_LONG 
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "includes/get_next_line/get_next_line.h"
+#include "includes/ft_printf/ft_printf.h"
+#include "includes/libft/libft.h"   
+
+typedef struct s_game
+{
+    char **map;
+} t_game;
+
+
+#endif
--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -1,28 +1,68 @@
-#include <mlx.h>
-#include <stdlib.h>
 
-int main()
+#include "../so_long.h"
+
+void check_file(char *file)
 {
-    void *mlx;
-    void *win;
+    int i = 0;
+    int fd;
 
-    // 1. تهيئة MiniLibX
-    mlx = mlx_init();
-    if (!mlx)
+    fd = open(file, O_RDONLY);
+    if(fd < 0)
     {
-        return 1;
+        ft_printf("%s", "error3\n");
+        exit(0);
     }
-
-    // 2. إنشاء نافذة
-    win = mlx_new_window(mlx, 800, 600, "MiniLibX Test");
-    if (!win)
+    while(file[i])
     {
-        free(mlx);
-        return 1;
+        i++;
     }
+    if(file[i- 1] != 'r' || file[i - 2] != 'e' || file[i - 3] != 'b' || file[i - 4] != '.')
+    {
+        ft_printf("%s", "error2\n");
+        exit(0);
+    }
+    close(fd);
+    return;
+}
 
-    // 3. تشغيل الحلقة الرئيسية للـ MiniLibX
-    mlx_loop(mlx);
+char **init_map(char *file, t_game *game)
+{
+    int fd;
+    char **map;
+    char *line;
+    int i = 0;
+    fd = open(file, O_RDONLY);
+    line = get_next_line(fd);
+    map = (char **)malloc(ft_strlen(line) + 1);
+    while(line)
+    {
+        map[i] = ft_strdup(line);
+        free(line);
+        line = get_next_line(fd);
+        i++;
+    }
+    map[i] = NULL;
+    free(line);
+    game->map = map;
+}
 
+int main(int ac, char **av)
+{
+    t_game game;
+    char **map;
+    if(ac != 2)
+    {
+        ft_printf("%s", "error1\n");
+        return 0;
+    }
+    check_file(av[1]);
+    init_map(av[1], &game);
+    check_map();
+    int i = 0;
+    while (game.map[i])
+    {
+        ft_printf("%s", game.map[i]);
+        i++;
+    }
     return 0;
 }

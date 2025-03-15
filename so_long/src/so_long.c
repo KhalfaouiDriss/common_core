b3e70ebe8ca72a246246be913df0c6c75be8e53f
Driss Khalfaoui Hassani <dkhalfao@c3r3p2.1337.ma>
2025-03-15 20:55:11 +0000

    Common_Core

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -49,85 +49,85 @@ void render_game(t_game *game)
     }
 }
 
+int find_move(t_game *game,int keycode, int *dx, int *dy)
+{
+    int Q;
+
+    Q = 0;
+    (*dx = 0, *dy = 0);
+    if (keycode == 65307)
+        ft_error(game);
+    if (keycode == 119) // W
+        (*dx = -1, Q = 1);
+    else if (keycode == 115) // S
+        (*dx = 1, Q = 1);
+    else if (keycode == 97) // A
+        (*dy = -1, Q =1);
+    else if (keycode == 100) // D
+        (*dy = 1, Q = 1);
+    return Q;
+}
+
+void reinit_game(t_game *game, int *new_x, int *new_y)
+{
+    if (game->map[*new_x][*new_y] == 'C')
+        game->collectible_count--;
+    if (game->collectible_count == 0)
+        game->exit_c = game->exit_o; 
+    if (game->is_exit)
+        game->map[game->player_x][game->player_y] = 'E';
+    else
+        game->map[game->player_x][game->player_y] = '0';
+    if (game->map[*new_x][*new_y] == 'E')
+    {
+        if(game->collectible_count == 0)
+            ft_error(game);
+        game->is_exit = 1;
+    }
+    else
+        game->is_exit = 0;
+    (game->player_x = *new_x, game->player_y = *new_y);
+    game->map[*new_x][*new_y] = 'P';
+}
+
 int move_player(int keycode, t_game *game)
 {
-    if (keycode == 119) // W
-    {
-        if(game->map[game->player_x - 1][game->player_y] == '1')
-            return (0);
-        if(game->map[game->player_x - 1][game->player_y] == 'C')
-            game->collectible_count++;
-        if(game->map[game->player_x - 1][game->player_y] == 'E')
-            game->is_exit = 1;
-        game->map[game->player_x][game->player_y] = '0';
-        if(game->is_exit)
-        {
-            game->map[game->player_x][game->player_y] = 'E';
-            game->is_exit = 0;
-        }
-        game->map[--game->player_x][game->player_y] = 'P';
-    }
+    int (dx), (dy), (new_x), (new_y), (Q);
 
-    if (keycode == 115) // S
-    {
-        if(game->map[game->player_x + 1][game->player_y] == '1')
-            return (0);
-
-        if(game->map[game->player_x + 1][game->player_y] == 'C')
-            game->collectible_count++;
-        if(game->map[game->player_x + 1][game->player_y] == 'E')
-            game->is_exit = 1;
-        game->map[game->player_x][game->player_y] = '0';
-        if(game->is_exit)
-        {
-            game->map[game->player_x][game->player_y] = 'E';
-            game->is_exit = 0;
-        }
-        game->map[++game->player_x][game->player_y] = 'P';
-    }
-
-    if (keycode == 97) // D
-    {
-        if(game->map[game->player_x][game->player_y - 1] == '1')
-            return (0);
-        if(game->map[game->player_x][game->player_y - 1] == 'C')
-            game->collectible_count++;
-        if(game->map[game->player_x][game->player_y - 1] == 'E')
-            game->is_exit = 1;
-        game->map[game->player_x][game->player_y] = '0';
-       if(game->is_exit)
-        {
-            game->map[game->player_x][game->player_y] = 'E';
-            game->is_exit = 0;
-        }
-        game->map[game->player_x][--game->player_y] = 'P';
-    }
-
-    if (keycode == 100) // A
-    {
-        if(game->map[game->player_x][game->player_y + 1] == '1')
-            return (0);
-        if(game->map[game->player_x][game->player_y + 1] == 'C')
-            game->collectible_count++;
-        if(game->map[game->player_x][game->player_y + 1] == 'E')
-            game->is_exit = 1;
-        game->map[game->player_x][game->player_y] = '0';
-        if(game->is_exit)
-        {
-            game->map[game->player_x][game->player_y] = 'E';
-            game->is_exit = 0;
-        }
-        game->map[game->player_x][++game->player_y] = 'P';
-    }
-
+    if(keycode > 0)
+        Q = find_move(game, keycode, &dx, &dy);
+    else   
+        return (0);
+    (new_x = game->player_x + dx, new_y = game->player_y + dy);
+    if (game->map[new_x][new_y] == '1')
+        return (0); 
+    if(Q)
+        ft_printf("Move : %d\n", ++game->move_count);
+    reinit_game(game, &new_x, &new_y);
     render_game(game);
     return (0);
+}
+
+int	exit_game(t_game *game)
+{
+	game->map = NULL;
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+		game->mlx = NULL;
+	}
+    ft_error(game);
+	return (0);
 }
 
 void so_long(t_game *game)
 {
     initial_imgs(game);
     render_game(game);
+    mlx_hook(game->win, 17, 0, exit_game, game);
     mlx_key_hook(game->win, move_player, game);
     mlx_loop(game->mlx);
 }

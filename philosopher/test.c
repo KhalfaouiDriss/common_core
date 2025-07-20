dbfa25d6cf3e1c6c2be90da1b31a17c8e025bc04
driss <driss@example.com>
2025-07-20 07:54:51 +0100

    Common_Core

--- philosopher/test.c
+++ philosopher/test.c
@@ -5,65 +5,135 @@
 #include <unistd.h>
 #include <stdio.h>
 
-typedef struct test
+typedef struct s_data t_data;
+
+typedef struct s_mutexs
 {
-    int l;
-    pthread_mutex_t m;
-    int k;
-} t_test;
+    pthread_mutex_t lock_eat;
+    // pthread_mutex_t lock_eat;
+} t_mutexs;
+
+typedef struct s_philo
+{
+    int id;
+    pthread_t th;
+    t_data *data;
+    int t_eat;
+    int t_die;
+    int t_sleep;
+    int n_philos;
+    pthread_mutex_t right_fork;
+    pthread_mutex_t *left_fork;
+    t_mutexs *mutexs;
+
+} t_philo;
+
+typedef struct s_data
+{
+    t_philo th[200];
+    int t_eat;
+    int t_die;
+    int t_sleep;
+    int n_philos;
+    t_mutexs *mutexs;
+
+} t_data;
 
 
-void mtx_lock(int *l)
+void rot(t_philo *philo)
 {
-    if(*l)
-        while (*l)
-            ;
-    if(!(*l))
-        *l = 1;
-}
-
-void mtx_unlock(int *l)
-{
-    *l = 0; 
-}
-
-void *rot(void *test)
-{
-    int i = 0, j = 0, k;
-    t_test *tt = (t_test *)test;
-    pthread_mutex_lock(&tt->m);
-    // mtx_lock(&tt->l);
-    tt->k++;
-    while(i <= 10)
-    {
-        printf("th[%d] : %d\n", tt->k, j);
-        j++;
-        i++;
-        sleep(1);           
+    if (philo->id % 2 == 0)
+        sleep(1);
+    if (philo->id % 2 == 0) {
+        pthread_mutex_lock(&philo->right_fork);
+        printf("philo[%d] : has taken right fork\n", philo->id + 1);
+        pthread_mutex_lock(philo->left_fork);
+        printf("philo[%d] : has taken left fork\n", philo->id + 1);
+    } else {
+        pthread_mutex_lock(philo->left_fork);
+        printf("philo[%d] : has taken left fork\n", philo->id + 1);
+        pthread_mutex_lock(&philo->right_fork);
+        printf("philo[%d] : has taken right fork\n", philo->id + 1);
     }
-    pthread_mutex_lock(&tt->m);
-    //  mtx_unlock(&tt->l);
+    pthread_mutex_lock(&philo->mutexs->lock_eat);
+    printf("philo[%d] : is eating\n", philo->id + 1);
+    sleep(1);
+    printf("philo[%d] : finish eating\n", philo->id + 1);
+    pthread_mutex_unlock(&philo->mutexs->lock_eat);
+
+    pthread_mutex_unlock(&philo->right_fork);
+    pthread_mutex_unlock(philo->left_fork);
+    printf("philo[%d] : is sleeping\n", philo->id + 1);
+    sleep(1);
+    printf("philo[%d] : is thinking\n", philo->id + 1);
+    usleep(1);
+}
+
+
+void *simulation(void *ph)
+{
+    t_philo *philo = (t_philo *)ph;
+    if(philo->id == 0)
+        usleep(1);
+    while (1)
+        rot((t_philo *)philo);
+}
+
+int init_data(t_data *data)
+{
+    t_mutexs *mutexs;
+
+    data->t_die = 210;
+    data->t_eat = 900;
+    data->t_sleep = 900;
+    data->n_philos = 5;
+    mutexs = malloc(sizeof(t_mutexs));
+    if (!mutexs)
+        return 0;
+    if (pthread_mutex_init(&mutexs->lock_eat, NULL) != 0)
+        return 0;
+    data->mutexs = mutexs;
+    return 1;
+}
+
+
+void create_philos(t_data *data)
+{
+    int i = 0;
+    while (data->n_philos > i)
+    {
+        data->th[i].id = i;
+        data->th[i].data = data;
+        data->th[i].mutexs = data->mutexs;
+        data->th[i].t_die = data->t_die;
+        data->th[i].t_eat = data->t_eat;
+        data->th[i].t_sleep = data->t_sleep;
+        pthread_mutex_init(&data->th[i].right_fork, NULL);
+        if(i > 0)
+            data->th[i].left_fork = &data->th[i - 1].right_fork; 
+        else
+            data->th[i].left_fork = &data->th[data->n_philos - 1].right_fork; 
+
+        pthread_create(&data->th[i].th, NULL, simulation, (void *)&data->th[i]);
+
+        i++;
+    }
+    i = 0;
+    while (data->n_philos > i)
+    {
+         pthread_join(data->th[i].th, NULL);
+         i++;
+    }
 }
 
 int main()
 {
-    pthread_t th[6];
-    pthread_mutex_t m;
-    
-    pthread_mutex_init(&m, NULL);
-    t_test test;
-    test.l = 0;
-    test.k = 0;
-    pthread_create(&th[0], NULL, rot, (void *)&test);
-    pthread_create(&th[1], NULL, rot, (void *)&test);
-    pthread_create(&th[2], NULL, rot, (void *)&test);
-    pthread_create(&th[3], NULL, rot, (void *)&test);
-    pthread_create(&th[4], NULL, rot, (void *)&test);
-    pthread_create(&th[5], NULL, rot, (void *)&test);
-    pthread_join(th[0], NULL);
-    pthread_join(th[1], NULL);
-    pthread_join(th[2], NULL);
-    pthread_join(th[3], NULL);
-    pthread_join(th[4], NULL);
-    pthread_join(th[5], NULL);
+    t_data data;
+    if(!init_data(&data))
+    {
+        printf("====\n");
+        return 0;
+    }
+    create_philos(&data);
+
 }

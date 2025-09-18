59cc73773964e4fa3e5604c233ab42c431c088b6
Driss Khalfaoui Hassani <drisspaca4@gmail.com>
2025-09-18 10:43:08 +0100

    Common_Core

--- CPP-Models/cpp01/ex04/main.cpp
+++ CPP-Models/cpp01/ex04/main.cpp
@@ -2,18 +2,55 @@
 #include <fstream>
 #include <string>
 
-int main(int ac, char **av)
+std::string replace(std::string content, const std::string &s1, const std::string &s2)
 {
-    if(ac != 4)
-        return 1;
-    
-    std::ifstream infile(av[1]);
-    std::ofstream outfile("test.txt");
-    std::string line;
+    size_t pos = 0;
+    if (s1.empty() || s1 == s2)
+        return content;
+    while ((pos = content.find(s1, pos)) != std::string::npos)
+    {
+        content.erase(pos, s1.length());
+        content.insert(pos, s2);
+        pos += s2.length();
+    }
+    return content;
+}
 
+bool getContent(const char *file, std::string &content)
+{
+    std::ifstream infile(file);
+    if (!infile.is_open())
+        return false;
+
+    std::string line;
+    content.clear();
     while (std::getline(infile, line))
     {
-        outfile << line << "\n";
+        content.append(line);
+        content.append("\n");
     }
-    
+    return true;
 }
+
+int main(int ac, char **av)
+{
+    if (ac != 4)
+        return 1;
+
+    std::string content;
+    if (!getContent(av[1], content))
+    {
+        std::cerr << "Cannot open file\n";
+        return 1;
+    }
+
+    std::string newStr = replace(content, av[2], av[3]);
+    std::string newFile = std::string(av[1]) + ".replace";
+
+    std::ofstream outfile(newFile.c_str());
+    if (!outfile.is_open())
+        return 1;
+
+    outfile << newStr;
+}
+

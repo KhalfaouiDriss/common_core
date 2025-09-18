71250e68edd7d2c1627b1ff910baba3023fd3fe6
Driss Khalfaoui Hassani <drisspaca4@gmail.com>
2025-09-18 10:52:06 +0100

    Common_Core

---
+++ CPP-Models/cpp01/ex05/Harl.cpp
@@ -0,0 +1,30 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+
+void Harl::debug()
+{
+
+}
+
+void Harl::info()
+{
+
+}
+
+void Harl::warning()
+{
+
+}
+
+void Harl::error()
+{
+
+}
+
+Harl::~Harl()
+{
+}

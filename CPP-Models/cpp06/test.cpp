03b37f01b625efde4433acb672a2a371f820ab7d
Driss <drisspaca4@gmail.com>
2026-02-09 06:19:27 +0100

    cpp06

--- CPP-Models/cpp06/test.cpp
+++ CPP-Models/cpp06/test.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
 
-class Base {
+class Animal {
 public:
-    virtual ~Base() {}
-    virtual void speak() { std::cout << "I am Base\n"; }
+    virtual ~Animal() {}
+    virtual void speak() { std::cout << "I am Animal\n"; }
 };
 
-class Child : public Base {
+class Dog : public Animal {
 public:
-    void speak() override { std::cout << "I am Child\n"; }
-    void special() { std::cout << "Child only!\n"; }
+    void speak() override { std::cout << "I am Dog\n"; }
+    void special() { std::cout << "I am Dog!\n"; }
+};
+
+class cat : public Animal {
+public:
+    void speak() override { std::cout << "I am cat\n"; }
+    void special() { std::cout << "I am cat!\n"; }
 };
 
 int main() {
     try
     {
-        Base a;
-        Child& c = dynamic_cast<Child&>(a);
-        std::cout << "success\n";
-        // c.special(); 
-        // c.speak();
-        (void)c;
+        Animal* An = new Dog();
+        Dog* StaticDog = static_cast<Dog*>(An);
+        StaticDog->speak();
+        Dog* DynamicDog = dynamic_cast<Dog*>(An);
+        if(DynamicDog)
+            std::cout << "Animal is dog\n";
+        void* address = reinterpret_cast<void*>(An);
+        std::cout << "An add :" << An << " | Memory Address in integer format: " << address << std::endl;
+        std::string str = "hello";
+        std::cout << reinterpret_cast<std::string*>(&str) << std::endl;
+        std::cout << &str;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
-    
 }
+
+

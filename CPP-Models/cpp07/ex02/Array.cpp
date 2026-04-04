326170bafd229936909392f043647114954982ac
driss <drisspaca4@gmail.com>
2026-04-04 12:42:21 +0000

    cpp07

--- CPP-Models/cpp07/ex02/Array.cpp
+++ CPP-Models/cpp07/ex02/Array.cpp
@@ -1,4 +1,37 @@
-    Array();
-    Array(unsigned int n);
-    Array(const Array& other);
-    Array& operator=(const Array& other);
+#ifndef ARRAY_TPP
+#define ARRAY_TPP
+
+template <typename T>
+class Array
+{
+private:
+    unsigned int _size;
+    T* _elements;
+public:
+    Array() : _size(0), _elements(NULL) {}
+    Array(unsigned int n)
+    {
+        _elements = new T[n]();
+    }
+    // Array(const Array& other)
+    // {
+        
+    // }
+    // Array& operator=(const Array& other)
+    // {
+        
+    // }
+
+    T* operator[](const unsigned int i)
+    {
+        if (i < 0 || i >= _size)
+            throw 1;
+        return _elements[i];
+    }
+
+
+    // ~Array();
+};
+
+
+#endif
--- CPP-Models/cpp07/ex02/Array.hpp
+++ CPP-Models/cpp07/ex02/Array.hpp
@@ -1,21 +1,74 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+
 template <typename T>
 class Array
 {
 private:
-    T* _elements;
     unsigned int _size;
+    T* _elements;
+
 public:
-    Array();
-    Array(unsigned int n);
-    Array(const Array& other);
-    Array& operator=(const Array& other);
+    class OutOfRangeException : public std::exception
+    {
+    public:
+        virtual const char* what() const throw()
+        {
+            return "Array index out of bounds";
+        }
+    };
 
+    Array() : _size(0), _elements(new T[0]) {}
 
-    ~Array();
+    Array(unsigned int n) : _size(n), _elements(new T[n]()) {}
+
+    Array(const Array& other) : _size(other._size), _elements(new T[other._size])
+    {
+        unsigned int i = -1; 
+        while (++i < _size)
+            _elements[i] = other._elements[i];
+    }
+
+    Array& operator=(const Array& other)
+    {
+        if (this != &other)
+        {
+            T* newElements = new T[other._size];
+            unsigned int i = -1;
+            while (++i < other._size)
+                newElements[i] = other._elements[i];
+            delete[] _elements;
+            _elements = newElements;
+            _size = other._size;
+        }
+        return *this;
+    }
+
+    T& operator[](unsigned int index)
+    {
+        if (index >= _size)
+            throw OutOfRangeException();
+        return _elements[index];
+    }
+
+    const T& operator[](unsigned int index) const
+    {
+        if (index >= _size)
+            throw OutOfRangeException();
+        return _elements[index];
+    }
+
+    unsigned int size() const
+    {
+        return _size;
+    }
+
+    ~Array()
+    {
+        delete[] _elements;
+    }
 };
-
 
 #endif

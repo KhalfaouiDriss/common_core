94ac35d71f681c106e62e4aa4d2ed04239e83684
Driss Khalfaoui Hassani <drisspaca4@gmail.com>
2026-04-13 13:06:43 +0100

    cpp09

--- CPP-Models/cpp09/ex01/RPN.cpp
+++ CPP-Models/cpp09/ex01/RPN.cpp
@@ -1,8 +1,12 @@
 #include "RPN.hpp"
 
+RPN::RPN() : _expression("") {
+}
+
 RPN::RPN(std::string expression) : _expression(expression)
 {
     CheckExpr();
+    ProcessStack();
 }
 
 int RPN::CheckExpr()
@@ -16,6 +20,60 @@ int RPN::CheckExpr()
         it++;
     }
     return 0;
+}
+
+void RPN::ProcessStack()
+{
+    for (std::string::iterator it = _expression.begin(); it != _expression.end(); ++it)
+    {
+        if (std::isspace(*it))
+            continue;
+
+        if (std::isdigit(*it))
+        {
+            _stk.push(*it - '0');
+        }
+        else if (*it == '+' || *it == '-' || *it == '*' || *it == '/')
+        {
+            if (_stk.size() < 2)
+                throw std::runtime_error("Error");
+
+            int b = _stk.top(); 
+            _stk.pop();
+            int a = _stk.top();
+            _stk.pop();
+
+            if (a < 0 || a > 10 || b < 0 || b > 10)
+                throw std::runtime_error("Error");
+
+            if (*it == '+') _stk.push(a + b);
+            else if (*it == '-') _stk.push(a - b);
+            else if (*it == '*') _stk.push(a * b);
+            else if (*it == '/') {
+                if (b == 0) throw std::runtime_error("Error: Division by zero");
+                _stk.push(a / b);
+            }
+        }
+        else
+            throw std::runtime_error("Error");
+    }
+
+    if (_stk.size() != 1)
+        throw std::runtime_error("Error");
+
+    std::cout << _stk.top() << std::endl;
+}
+
+RPN::RPN(const RPN &other) {
+    *this = other;
+}
+
+RPN &RPN::operator=(const RPN &other) {
+    if (this != &other) {
+        this->_expression = other._expression;
+        this->_stk = other._stk;
+    }
+    return *this;
 }
 
 RPN::~RPN()
--- CPP-Models/cpp09/ex01/RPN.hpp
+++ CPP-Models/cpp09/ex01/RPN.hpp
@@ -3,15 +3,32 @@
 #include <iostream>
 #include <exception>
 #include <string>
+#include <stack>
 #include <algorithm>
 
 class RPN
 {
 private:
     std::string _expression;
+    std::stack<int> _stk;
+
 public:
+    // 1. Default Constructor
+    RPN();
+
+    // Parameterized Constructor
     RPN(std::string expression);
+
+    // 2. Copy Constructor
+    RPN(const RPN &other);
+
+    // 3. Assignment Operator
+    RPN &operator=(const RPN &other);
+
     int CheckExpr();
+    void ProcessStack();
+
+    // 4. Destructor
     ~RPN();
 };
 

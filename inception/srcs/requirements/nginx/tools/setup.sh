be877273313bd32631e3b28c6c15134b86799220
KhalfaouiDriss <drisspaca4@gmail.com>
2026-05-06 11:38:10 +0000

    inception

--- inception/srcs/requirements/nginx/tools/setup.sh
+++ inception/srcs/requirements/nginx/tools/setup.sh
@@ -5,4 +5,6 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -out /etc/nginx/ssl/cert-out.crt \
     -subj "/C=MA/L=Khouribga/O=1337/CN=dkhalfao.42.fr"
 
+nginx -t
+
 exec nginx -g "daemon off;"
